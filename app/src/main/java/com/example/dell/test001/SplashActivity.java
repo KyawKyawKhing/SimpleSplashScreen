d7de4db24365587a9fac29509c24462e175085dc
Kyaw Khing <sawkyaw1111.sk1@gmail.com>
2018-03-09 14:21:56 +0630

    first commit

---
+++ app/src/main/java/com/example/dell/test001/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.dell.test001;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash);
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
